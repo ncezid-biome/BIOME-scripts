1313fa04366359239e2e6428e64d1d9038149d89
Griswold <ycj5@monolith3.biotech.cdc.gov>
2020-11-30 16:01:42 -0500

    Added comments. Added central read directory.

--- qsub_arrays/launch_assembleGenome_split.sh
+++ qsub_arrays/launch_assembleGenome_split.sh
@@ -1,39 +1,42 @@
 #!/bin/bash -l
 
 #-------------------------------------------------------------------------------------------------
-# This is a qsub array job script whichs downloads a large batch of SRA data.
-# The ordered columns of the required CSV import are: Key,WGS_id,NCBI_ACCESSION,SRR_id. The second
-# and fourth columns are the only columns which require content; the others can be blank.
-# Output files will have the named prefix structure of WGSID_SRRID.
-# Please adjust the user id of line number #69 to match your user id.
+# This is a qsub array job script whichs assembles a large batch of raw read data with Shovill.
+# The ordered columns of the required CSV import are: READ1_NAME,READ2_NAME,STRAIN_ID,MIN_COVERAGE.
+# Output files will have the named prefix structure of STRAIN_ID.
 #-------------------------------------------------------------------------------------------------
 
 # Read ARGV
 INPUT_CSV=$1
 SLOTS_PER_JOB=1 # manually change this as needed
 PARENTDIR=$2
+READDIR=$3
 
-if [ "INPUT_CSV" == "" ]; then
+# If there is not enough arguments, exit and display usage
+if [ "$READDIR" == "" ]; then
   echo "run shovill - trims, assembles, runs read correction; formats names, sets cov cutoff."
   echo "Set the cov cutoff to coverage calculated by CG pipeline divided by ten whichever is smaller since shovill downsamples to 100X."
-  echo "1 = Read 1 , 2 = Read 2, 3 = Strain, 4 = min coverage"
-  echo "Usage: $0 [STRAIN-LIST].csv [OUTPUT-DIR]"
+  echo "The ordered columns of the required CSV import are: READ1_NAME,READ2_NAME,STRAIN_ID,MIN_COVERAGE."
+  echo "The [READDIR] should contain both R1 and R2 fastq.gz files of STRAIN_IDs in CSV."
+  echo "Usage: $0 [STRAIN-LIST].csv [OUTPUT-DIR] [READDIR]"
   exit 1;
 fi
 
 mkdir $PARENTDIR
 
+# Create working temporary directory
 TMP=$(mktemp --tmpdir='.' --directory qsubShovill.XXXXXXXX)
 mkdir -p $TMP/log
 echo "tmp dir is $TMP "
 
+# Set up array control structure
 CTRL_FILE="$TMP/array.txt"
 cp $INPUT_CSV $CTRL_FILE
 echo "CTRL_FILE is $CTRL_FILE"
 
 
 qsub -N Shovill -q all.q -o $TMP/log -j y -pe smp $SLOTS_PER_JOB -V -cwd -t 1-$(cat $CTRL_FILE | wc -l) \
-  -v "CTRL_FILE=$CTRL_FILE","PARENTDIR=$PARENTDIR" <<- "END_OF_SCRIPT"
+  -v "CTRL_FILE=$CTRL_FILE","PARENTDIR=$PARENTDIR","READDIR=$READDIR" <<- "END_OF_SCRIPT"
   #!/bin/bash -l
   set -e
   source /etc/profile.d/modules.sh
@@ -42,8 +45,9 @@ qsub -N Shovill -q all.q -o $TMP/log -j y -pe smp $SLOTS_PER_JOB -V -cwd -t 1-$(
   module load trimmomatic velvet/1.2.10 seqtk/1.3
 
   which shovill
+  shovill --check
 
-  # Set up filenames
+  # Retrieve column values from INPUT_CSV
   STRAIN=$(sed -n ${SGE_TASK_ID}p $CTRL_FILE | awk -F',' '{print $3}')
   MINCOV=$(sed -n ${SGE_TASK_ID}p $CTRL_FILE | awk -F',' '{print $4}')
   READ1=$(sed -n ${SGE_TASK_ID}p $CTRL_FILE | awk -F',' '{print $1}')
@@ -56,9 +60,9 @@ qsub -N Shovill -q all.q -o $TMP/log -j y -pe smp $SLOTS_PER_JOB -V -cwd -t 1-$(
   echo "Shovill will be run on $STRAIN with minimum coverage of $MINCOV under $(hostname)"
   echo "Working directory is $outdir"
 
-  shovill --outdir "$outdir"/shovill --R1 $READ1 --R2 $READ2 --mincov $MINCOV --trim --namefmt "$STRAIN"_contig%05d 
+  shovill --outdir "$outdir"/shovill --R1 $READDIR/$READ1 --R2 $READDIR/$READ2 --mincov $MINCOV --trim --namefmt "$STRAIN"_contig%05d 
 
-  #rename files to include strain name
+  # Rename Shovill output files to include strain name
   mv $outdir/shovill $PARENTDIR/$STRAIN
   mv $PARENTDIR/$STRAIN/contigs.fa $PARENTDIR/$STRAIN/$STRAIN.contigs.fa;
   mv $PARENTDIR/$STRAIN/contigs.gfa $PARENTDIR/$STRAIN/$STRAIN.contigs.gfa;
